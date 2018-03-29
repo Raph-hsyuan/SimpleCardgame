dd74e9dbc3903c218677bbc4eadbae6d09c7347d
Shenyuan Huang <shenyuan.huang@etu.unice.fr>
2018-03-29 10:08:22 +0200

    CPA-1 create inventor simple

---
+++ src/test/java/cpa/inventeur/InventorTest.java
@@ -0,0 +1,33 @@
+package cpa.inventeur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class InventorTest {
+
+	Inventor inventor = new Inventor("tester");
+	
+	@Test
+	void testGetStatut() {
+		boolean expect = true;
+		assertEquals(expect,inventor.getStatut());
+	}
+	
+	@Test
+	void testSetBusy() {
+		inventor.setBusy();
+		boolean expect = false;
+		assertEquals(expect,inventor.getStatut());
+	}
+	
+	@Test
+	void testSetFree() {
+		inventor.setFree();
+		boolean expect = true;
+		assertEquals(expect,inventor.getStatut());
+	}
+	
+	
+
+}
